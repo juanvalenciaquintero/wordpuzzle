411edcf36cf4ea74062bff8c29b518b4d0f0167e
juanvalenciaquintero <juan140269@gmail.com>
2022-01-31 16:08:27 +0100

    update

--- index.php
+++ index.php
@@ -1,105 +1,67 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
     <title>Document</title>
 </head>
-<style>
-    #divJugador
-    {
-        width:100%;
-    }
-    #divJugador>div
-    {
-        width:200px;
-        height:100px;
-        margin: 100px auto;
-        display:flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-    }
-    #titulo
-    {
-        display:flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-    }
-    #titulo span, #crono span
-    {
-        text-align: center;
-        font-size:14px;
-    }
-    #btnJugar
-    {
-        width:80px;
-        font-weight: 400;
-        font-size: 16px;
-    }
-    #jugador
-    {
-        width:150px;
-    }
-    #btnJugador
-    {
-        width:80px;
-    }
-    #contenedor
-    {
-        width:170px;
-        margin:100px auto;
-        display:none;
-    }
-    #contenedor #palabras input
-    {
-        width:30px;
-        height:30px;
-        margin:0;
-        padding:0;
-        text-align: center;
-        font-weight: bold;
-        font-size: 24px;
-    }
-    #crono
-    {
-        text-align: center;
-        margin-top:10px;
-    }
-</style>
 <body>
-<div id="divJugador">
+<?php
+ //Conexion valquinter.com
+// $conexion = mysqli_connect('localhost','valquint_juan','42180200Az','valquint_juegos');
+
+//Conexión local
+$conexion = mysqli_connect('localhost','root','','diccionario');
+
+$loginVisible = 'style="display:none"';
+if (!isset($_POST['nombre']))
+{
+    $loginVisible = 'style="display:block"';
+}
+?>
+<div id="divJugador" <?php echo $loginVisible; ?>>
     <div>
-        <span>¿Quien eres?</span>
-        <input type="text" id="jugador">
-        <input type="button" id="btnJugador" value="Empezar" onclick="empezar();">
+        <form id="formLogin" method="post">
+            <span>¿Quien eres?</span>
+            <input type="text" id="jugador" name="nombre" value="<?php echo (isset($_POST['nombre']) ? $_POST['nombre'] : '');  ?>">
+            <input type="hidden" id="long" name="dificultad" value="<?php echo (isset($_POST['dificultad']) ? $_POST['dificultad'] : '');  ?>">
+            <input type="button" id="btnJugador" value="Empezar" onclick="empezar();">
+        </form>
     </div>
 </div>
+<?php
+
+?>
 <div id="contenedor">
     <div id="chivato"></div>
-    <div id="titulo"></div>
+    <div id="titulo">
+    <?php
+    if (isset($_POST['nombre']) && (isset($_POST['dificultad'])))
+    {
+        $record = 0;
+        $longitud = $_POST['dificultad'];
+        $sql = mysqli_query($conexion,"SELECT * FROM wordpuzzle_records WHERE longitud=" . $longitud);
+        if (mysqli_num_rows($sql)>0)
+        {
+            $result = mysqli_fetch_array($sql);
+            $record = $result['tiempo'];
+        }
+    ?>
+        <span id="jugador">Hola <?php  echo $_POST['nombre']; ?></span>
+        <br>
+        <span>El record actual para este nivel es de <?php echo $record; ?> segundos</span>
+        <input type="button" id="btnJugar" value="Jugar" onclick="jugar();">
+
+
+    </div>
     <div id="crono"></div>
     <div id="palabras">
     <?php
-    //Conexion valquinter.com
-    // $conexion = mysqli_connect('localhost','valquint_juan','42180200Az','valquint_juegos');
-    //Conexión local
-    $conexion = mysqli_connect('localhost','root','','diccionario');
-
-    $record = 0;
-    $sql = mysqli_query($conexion,"SELECT * FROM wordpuzzle_records WHERE longitud=5 ");
-    if (mysqli_num_rows($sql)>0)
-    {
-        $result = mysqli_fetch_array($sql);
-        $record = $result['tiempo'];
-    }
     echo "<input id='record' type='hidden' value='" . $record . "'>";
 
-    $sql = mysqli_query($conexion,"SELECT * FROM wordpuzzle_palabras WHERE longitud=5 ORDER BY RAND() LIMIT 1");
+    $sql = mysqli_query($conexion,"SELECT * FROM wordpuzzle_palabras WHERE longitud=$longitud ORDER BY RAND() LIMIT 1");
     $result = mysqli_fetch_array($sql);
     $palabra = str_split($result['palabra']);
 
@@ -123,6 +85,9 @@
 
 ?>
 </div>
+<?php
+}
+?>
 <div id="mensajes"></div>
 </div>
 </body>
@@ -133,9 +98,15 @@
         var fila = 0;
         var columna = 0;
 
+
         var selector = 'palabra_' + fila + '_' + columna;
+        var palabra = document.getElementById(selector);
         console.log(selector);
-        document.getElementById(selector).focus();
+        if (palabra)
+        {
+            document.getElementById(selector).focus();
+        }
+
 
     })();
 
@@ -167,23 +138,32 @@
         document.getElementById('divJugador').style.display="none";
         document.getElementById('contenedor').style.display="block";
         var nombre = document.getElementById('jugador').value;
-        var record = document.getElementById('record').value;
-        var html = '<span>Hola <span id="nombre">' + nombre + '</span></span><span>Pulsa el botón para empezar</span><input type="button" id="btnJugar" value="Jugar" onclick="jugar();">';
+        // var record = document.getElementById('record').value;
+        // var html = '<span>Hola <span id="nombre">' + nombre + '</span></span><span>Pulsa el botón para empezar</span><input type="button" id="btnJugar" value="Jugar" onclick="jugar();">';
+        var html = '<span>Hola <span id="nombre">' + nombre + '</span><br>';
+        html += '<form id="formDificultad" method="post"><select id="dificultad" name="dificultad" onchange="selectDif();"><option value="0">Selecciona la dificultad</option><option value="4">4 letras</option><option value="5">5 letras</option><option value="6">6 letras</option><option value="7">7 letras</option><option value="8">8 letras</option><option value="9">9 letras</option></select><input type="hidden" name="nombre" value="' + nombre + '"></form></span>';
 
-        if (record>0)
-        {
-            var min = parseInt(record/60);
-            var seg = record%60;
-            html += '<span>El record actual es ' + min + ':' + seg + '</span>';
-        }
-        html += '</span>';
+        // if (record>0)
+        // {
+        //     var min = parseInt(record/60);
+        //     var seg = record%60;
+        //     html += '<span>El record actual es ' + min + ':' + seg + '</span>';
+        // }
+        // html += '</span>';
         document.getElementById('titulo').innerHTML = html;
     }
 
     function jugar()
     {
+
         timer();
     }
+
+    function selectDif()
+    {
+        document.getElementById('formDificultad').submit();
+    }
+
     function checkLetra(event)
     {
         var id = event.target.id;
@@ -256,7 +236,7 @@
         } else if(aciertos==5)
         {
             var jugador = document.getElementById('jugador').innerHTML;
-            document.getElementById('mensajes').innerHTML = '¡Enhorabuena! Lo has conseguido';
+            document.getElementById('mensajes').innerHTML = '¡Enhorabuena! Lo has conseguido<br><input type="button" value="Volver a jugar" onclick="jugarOtra();">';
             console.log('Tiempo: ' + tiempo);
             clearTimeout(t);
             var jugador = document.getElementById('nombre').innerHTML;
@@ -277,10 +257,16 @@
             }));
         } else if(fila==6)
         {
-            document.getElementById('mensajes').innerHTML = 'Lo siento. La palabra era ' + objPalabra + '. Intentalo de nuevo';
+            document.getElementById('mensajes').innerHTML = 'Lo siento. La palabra era ' + objPalabra + '. Intentalo de nuevo<br><input type="button" value="Volver a jugar" onclick="jugarOtra();">';
             console.log('Tiempo: ' + tiempo);
             clearTimeout(t);
         }
+    }
+
+    function jugarOtra()
+    {
+
+        document.getElementById('formLogin').submit();
     }
 
     function inArray(needle, haystack) {
